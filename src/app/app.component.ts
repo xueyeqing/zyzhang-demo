137e89e17b5d6606ea4bee2cd41275e4db6ba154
zhangzhengyi <zhangtaihu101@163.com>
2019-10-22 14:35:54 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'zyzhang-demo';
-}
+export class AppComponent {}
--- src/app/demo/demo.module.ts
+++ src/app/demo/demo.module.ts
@@ -1,21 +1,13 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
 import {ComModule} from '../com/com.module';
 import {TestComponent} from './test/test.component';
 import {DemoRoutingModule} from './demo-routing.module';
-import {FormsModule} from '@angular/forms';
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [TestComponent],
   imports: [
-    CommonModule,
     ComModule,
     DemoRoutingModule,
-    BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule
   ]
 })
 export class DemoModule {
